ba7e425c75a6db4fd3078580864f6a31422cfaf5
root <root@e1c73ccd8f16>
2020-07-03 18:32:09 +0000

    Day 5
    I will definitely come back later in the summer, I had a ton of fun!

--- src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,7 +1,17 @@
 
 void setup() {
-
+PImage face = loadImage("frog.jpg"); 
+size(500,400);
+face.resize(500,400);
+image(face,0,0);
 }
 void draw() {
-
+  fill(mouseX,mouseY,mouseY);
+ellipse(135,95,100,100);
+fill(mouseX,mouseY,mouseX);
+ellipse(360,100,100,100);
+fill(mouseY,mouseX,mouseX);
+ellipse(360,100,10,10);
+fill(mouseY,mouseX,mouseY);
+ellipse(135,95,10,10);
 }
---
+++ src/_05_for_loops/_2_badgers/BADGER.java
@@ -0,0 +1,32 @@
+package _05_for_loops._2_badgers;
+
+public class BADGER {
+public static void main(String[] args) {
+	
+	for (int i = 0; i < 2; i++) {
+		
+	
+	for (int i1 = 0; i1 < 2; i1++) {
+		
+	
+	for (int i11 = 0; i11 < 12; i11++) {
+		
+	System.out.print("Badger ");
+	}
+	
+	System.out.println();
+	
+	for (int j = 0; j < 2; j++) {
+		
+	
+	System.out.println("Mushroom ");
+	}
+	
+		System.out.println();
+	}
+	
+	System.out.println("SNAKE!");
+}
+	}
+}
+
